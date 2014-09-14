3df220d1871e724877ac7abf882d736e56459136
LXMedia1 <anfrbo@gmail.com>
2014-09-14 21:32:12 +0200

    version

--- LexxersAIOCarry/Program.cs
+++ LexxersAIOCarry/Program.cs
@@ -6,7 +6,7 @@ namespace UltimateCarry
 {
 	class Program
 	{
-		public const int LocalVersion = 71;
+		public const int LocalVersion = 72;
 		public static Champion Champion;
 		public static Menu Menu;
 		public static Orbwalking.Orbwalker Orbwalker;
